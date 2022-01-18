dfe9f92b94143fc5557e4369f713d5327336d773
FitriKurniawati21 <97673624+FitriKurniawati21@users.noreply.github.com>
2022-01-18 17:28:03 +0700

    Tugas_1

---
+++ INSTAGRAM/app/src/main/java/com/Fitri/instagram/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.Fitri.instagram
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    internal override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
